d464ee04c799ffb3967547e13a8dcc53e3928d10
Gurram Babu <gurbabu@publicisgroupe.net>
2021-04-27 15:06:26 +0530

    New Files

---
+++ mavn_day1/src/main/java/day1/MultiplyTwoNumbers.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class MultiplyTwoNumbers {
+
+	public static int mul(int num1, int num2) {
+		return num1*num2;
+	}
+}
---
+++ mavn_day1/src/test/java/day1/MultiplyTwoNumbersTest.java
@@ -0,0 +1,14 @@
+package day1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MultiplyTwoNumbersTest {
+
+	@Test
+	void test() {
+		assertEquals(2,MultiplyTwoNumbers.mul(1,2));
+	}
+
+}
